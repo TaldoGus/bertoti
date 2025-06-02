51ad7380ba934ab23347401944dfdb9d287d8750
TaldoGus <xgustavopereirax@gmail.com>
2025-06-01 22:31:46 -0300

    Antipattern

---
+++ eng3/antipattern/Cachorro.java
@@ -0,0 +1,14 @@
+public class Cachorro extends Animal{
+
+    public String nome;
+
+    public String raca;
+
+
+    public Cachorro(String nome, String raca){
+        this.nome = nome;
+        this.raca = raca;
+    }
+
+    
+}
---
+++ eng3/antipattern/Teste.java
@@ -0,0 +1,10 @@
+public class Teste {
+    public static void main(String[] args) {
+        Cachorro obj1 = new Cachorro("Bidu", "Labrador");
+        Gato obj2 = new Gato("Garfield", "SRD");
+
+        obj1.andar();
+        obj2.comer();
+
+    }
+}
